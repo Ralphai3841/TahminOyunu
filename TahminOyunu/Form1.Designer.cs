182b1727f1a1689c95253187d0ce79998d6d8523
ralpd <ralpd@LAPTOP-FB9086OP>
2023-09-21 05:00:13 +0300

    Add project files.

---
+++ TahminOyunu/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace TahminOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.lstGuessedWords = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Location = new System.Drawing.Point(609, 383);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(161, 35);
+            this.btnNewGame.TabIndex = 0;
+            this.btnNewGame.Text = "New Game";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.Enabled = false;
+            this.txtGuess.Location = new System.Drawing.Point(348, 152);
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.Size = new System.Drawing.Size(181, 22);
+            this.txtGuess.TabIndex = 1;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Enabled = false;
+            this.btnCheck.Location = new System.Drawing.Point(382, 203);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(117, 23);
+            this.btnCheck.TabIndex = 2;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // lstGuessedWords
+            // 
+            this.lstGuessedWords.FormattingEnabled = true;
+            this.lstGuessedWords.ItemHeight = 16;
+            this.lstGuessedWords.Location = new System.Drawing.Point(48, 46);
+            this.lstGuessedWords.Name = "lstGuessedWords";
+            this.lstGuessedWords.Size = new System.Drawing.Size(224, 372);
+            this.lstGuessedWords.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstGuessedWords);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.txtGuess);
+            this.Controls.Add(this.btnNewGame);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.ListBox lstGuessedWords;
+    }
+}
+
